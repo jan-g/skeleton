2c1ec1da6090a9fe8e2a8d053478e41c6f303260
jan grant <jan.grant@oracle.com>
2018-08-30 15:12:03 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os.path
+from setuptools import setup, find_packages
+
+
+def read_file(fn):
+    with open(os.path.join(os.path.dirname(__file__), fn)) as f:
+        return f.read()
+
+setup(
+    name="skeleton",
+    version="0.0.1",
+    description="skeleton",
+    long_description=read_file("README.md"),
+    author="jang",
+    author_email="",
+    license=read_file("LICENCE.md"),
+
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'cmd = skeleton.cmd:main',
+        ],
+    },
+
+    install_requires=[
+                      "argcomplete",
+                     ],
+    tests_require=[
+                    "pytest",
+                    "flake8",
+                  ],
+)
