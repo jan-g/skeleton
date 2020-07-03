e43104c05206fe46fc0fb59d50707b32accb516f
jan grant <jan.grant@oracle.com>
2020-07-03 10:57:14 +0100

    Small tidy-up

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-import os.path
 from setuptools import setup, find_packages
 
-
-def read_file(fn):
-    with open(os.path.join(os.path.dirname(__file__), fn)) as f:
-        return f.read()
 
 setup(
     name="skeleton",
